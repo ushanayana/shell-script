32dc77570f2f3c9a002d6242263309a99f41e0c5
ushanayana <ushanayana@gmail.com>
2024-05-08 00:22:15 +0530

    welcome message

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: Hello $PERSON2,How are you?"
+echo "$PERSON2:: Hi $PERSON1,Iam fine, How r u?"
+echo "$PERSON1:: Iam fine too. How is your work?"
+echo "$PERSON2:: not bad,Iam thinking to upgrade to Devops"
