b243846e1f6baa7abb3ab3ba38491b735ef0044e
ushanayana <ushanayana@gmail.com>
2024-05-08 23:27:37 +0530

    modify message

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-echo "given number $NUMBER is greater than 10"
+   echo "given number $NUMBER is greater than 10"
 else 
 echo "given number $NUMBER is less than 10"
 fi
