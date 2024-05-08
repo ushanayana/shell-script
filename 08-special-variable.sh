30d38e3ffe08dd124fd9817bb3a46a5b91de0180
ushanayana <ushanayana@gmail.com>
2024-05-08 23:09:02 +0530

    modify message

--- 08-special-variable.sh
+++ 08-special-variable.sh
@@ -6,4 +6,5 @@ echo "Script name:$0"
 echo "current working directory:$PWD"
 echo "home directory of current user:$HOME"
 echo "which user is running this script:$USER"
- 
+ echo "hostname:$HOSTNAME"
+ echo "process ID of current shell-script:$$"
