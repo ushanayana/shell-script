dedaaeabee127b8d975569caed5d386e93d60663
ushanayana <ushanayana@gmail.com>
2024-05-08 23:05:37 +0530

    modify message

---
+++ 08-special-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all variables:$@"
+echo "number of variables:$#"
+echo "Script name:$0"
+echo "current working directory:$PWD"
+echo "home directory of current user:$HOME"
+echo "which user is running this script:$USER"
+ 
