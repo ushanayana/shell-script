69a05965db232b27b64cc1fb6fe1c88934f5b00e
ushanayana <ushanayana@gmail.com>
2024-05-08 22:44:18 +0530

    modify message

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "MURARI")
+
+#SIZE OF ABOVE ARRAY IS 3
+
+#index is always starts with 0
+
+echo "first movie is::${MOVIES[0]}"
+echo "first movie is::${MOVIES[1]}"
+echo "first movie is::${MOVIES[2]}"
