2ae151d6f02720f005feeefcf1b0d5e2c013b43b
ushanayana <ushanayana@gmail.com>
2024-05-09 23:54:12 +0530

    modify message

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,11 +10,21 @@ else
 echo "you are super user"
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
 echo "installation of mysql failure....."
 exit 1
+echo "installation of mysql is success..."
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+echo "installation of is failure,,,,"
+else
+echo "installation of git is sucess...."
 fi
 echo "please proceeding?.."
