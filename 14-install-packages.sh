171e8bbfd8d0267fb4aa75576f0348c0a8f8a3f8
ushanayana <ushanayana@gmail.com>
2024-05-13 00:18:23 +0530

    install-packages

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -2,7 +2,11 @@
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
-echo "run this script with root access" 
+then
+echo "run this script with root access"
+exit 1 
 else 
 echo "you r super user"
 fi
+
+echo "all packages:$@"
