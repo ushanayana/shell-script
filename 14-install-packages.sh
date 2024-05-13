e84dd5596a24a978827261de3f489f46c8528691
ushanayana <ushanayana@gmail.com>
2024-05-13 23:43:19 +0530

    loops

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ]
 then
 echo "run this script with root access"
@@ -12,6 +19,7 @@ fi
 for i in $@
 do 
 
-echo "all packages:$i"
+echo "package to install:$i"
+dnf list installed $i &>>$LOGFILE
 
 done
