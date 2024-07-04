0833d3ebe0def89e55a6b69152e2cb7207d97bb0
ushanayana <ushanayana@gmail.com>
2024-07-04 23:04:34 +0530

    current-script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops from current script"
 echo "Before calling current script:$COURSE"
 echo "Process ID of current script:$$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "Before calling current script:$COURSE"
