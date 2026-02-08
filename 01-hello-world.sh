6b077bcdb36dc715daa9fc346284da9255ac2793
ushanayana <ushanayana@gmail.com>
2026-02-08 19:56:40 +0530

    hello world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "hello,i am learning devops";
+echo "hello,i amlearningdevops";
--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hello Suresh,How are you?"
-echo "Suresh:: Hi Ramesh,Iam fine, How r u?"
-echo "Ramesh:: Iam fine too. How is your work?"
-echo "Suresh:: not bad,Iam thinking to upgrade to Devops"
+echo "Ramesh:: Hello suresh,how r u?"
+echo "Suresh:: hi Ramesh,iam fine,how r  u?"
+echo "Ramesh:: iam fine,how is your work?"
+echo "Suresh:: not bad,but iam upgrading my skill with DEVOPS"
